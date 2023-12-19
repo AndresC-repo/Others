2829cc5aa77f20a5c7777a57bf09a9b35263e55f
Andrés <75669936+AndresC-repo@users.noreply.github.com>
2023-12-19 14:44:04 +0100

    pydantuc

--- Others/Configuration/load_config.py
+++ Others/Configuration/load_config.py
@@ -7,11 +7,12 @@ from Configuration.pydantic_base import Config_file
 def main() -> None:
     """Main function."""
 
-    # Read data from a JSON file
+    # Read data from a JSON file -> multiple configurations in one json file
     with open("./data.json") as file:
         data = json.load(file)
         Config_file: List[Config_file] = [Config_file(**item) for item in data]
         print(Config_file[0])
+        # Config_file = Config_file(**data)  # One config
 
 
 if __name__ == "__main__":
